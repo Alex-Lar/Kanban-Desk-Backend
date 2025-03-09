19c48a50b647241021b3205644810c639c1348aa
Alexander Larin <doremilarin@gmail.com>
2025-03-09 15:53:36 +0300

    feat: setup express and trpc servers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-console.log('Hello World')
+import express, { Request, Response } from 'express'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { trpcRouter } from './trpc'
+const app = express()
 
-console.log("From Kanban App Backend");
+const PORT = 3000
+
+app.get('/ping', (req: Request, res: Response) => {
+  res.send('PING')
+})
+
+app.use("/trpc", trpcExpress.createExpressMiddleware({
+  router: trpcRouter
+}));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`)
+})
